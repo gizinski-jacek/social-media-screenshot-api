ba3937dd32c0ca9bb0a2a2557837356acfc11592
Jacek Gizinski <jacektrg+github@gmail.com>
2025-02-05 20:18:33 +0100

    Cleanup

--- src/screenshot/screenshot.module.ts
+++ src/screenshot/screenshot.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ScreenshotController } from './screenshot.controller';
-import { ScreenshotService } from './screenshot.service';
 import { BlueskyModule } from './bluesky/bluesky.module';
 
 @Module({
   imports: [BlueskyModule],
   controllers: [ScreenshotController],
-  providers: [ScreenshotService],
+  providers: [],
 })
 export class ScreenshotModule {}
