116ccf3bd65a8aa070d0055bd9604b0c86ea88dd
Jacek Gizinski <jacektrg+github@gmail.com>
2025-02-05 20:10:32 +0100

    Importing cloudinary provider and screenshot module.
    Setting up nestjs ConfigModule to load env vars from file.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ApiController, AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScreenshotModule } from './screenshot/screenshot.module';
+import { ConfigModule } from '@nestjs/config';
+import { CloudinaryProvider } from './cloudinary/cloudinary.provider';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), ScreenshotModule],
   controllers: [AppController, ApiController],
-  providers: [AppService],
+  providers: [AppService, CloudinaryProvider],
 })
 export class AppModule {}
