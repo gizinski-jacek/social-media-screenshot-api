90f5fe46e79dd815fb493e7fd5c3b55b05f922e4
Jacek Gizinski <jacektrg+github@gmail.com>
2025-02-06 21:03:02 +0100

    Importing Twitter module

--- src/screenshot/screenshot.module.ts
+++ src/screenshot/screenshot.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ScreenshotController } from './screenshot.controller';
 import { BlueskyModule } from './bluesky/bluesky.module';
+import { TwitterModule } from './twitter/twitter.module';
 
 @Module({
-  imports: [BlueskyModule],
+  imports: [BlueskyModule, TwitterModule],
   controllers: [ScreenshotController],
   providers: [],
 })
