990df6a84f43898e1b56446cf2c0d29522fcd886
Jacek Gizinski <jacektrg+github@gmail.com>
2025-02-08 21:14:52 +0100

    Importing new modules.

--- src/screenshot/screenshot.module.ts
+++ src/screenshot/screenshot.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ScreenshotController } from './screenshot.controller';
 import { BlueskyModule } from './bluesky/bluesky.module';
 import { TwitterModule } from './twitter/twitter.module';
+import { FacebookModule } from './facebook/facebook.module';
+import { InstagramModule } from './instagram/instagram.module';
 
 @Module({
-  imports: [BlueskyModule, TwitterModule],
+  imports: [BlueskyModule, TwitterModule, FacebookModule, InstagramModule],
   controllers: [ScreenshotController],
   providers: [],
 })
