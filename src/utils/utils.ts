669ef4868c6add82223433680e68fd623b2ed0b1
Jacek Gizinski <jacektrg+github@gmail.com>
2025-02-14 20:13:11 +0100

    Extracting function creating filename

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,12 @@
+export const createFilename = (
+  handle: string,
+  postId: string,
+  cd: number = 0,
+): string => {
+  const filename = `${handle}_${postId}_cd_${cd}_${createTimestamp()}.jpeg`;
+  return filename;
+};
+
 export const createTimestamp = (): string => {
   const timestamp = new Date()
     .toISOString()
