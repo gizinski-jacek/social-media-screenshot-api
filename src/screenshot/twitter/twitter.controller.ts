b16a3eca948ba1556fd0e0860af34c2a0501875b
Jacek Gizinski <jacektrg+github@gmail.com>
2025-02-14 20:16:55 +0100

    Moving query destructuring and puppeteer scraping logic to service file.

--- src/screenshot/twitter/twitter.controller.ts
+++ src/screenshot/twitter/twitter.controller.ts
@@ -1,99 +1,16 @@
-import {
-  Controller,
-  Get,
-  HttpException,
-  HttpStatus,
-  Query,
-  UsePipes,
-} from '@nestjs/common';
+import { Controller, Get, Query, UsePipes } from '@nestjs/common';
 import { TwitterService } from './twitter.service';
-import puppeteer, { Browser, Page } from 'puppeteer';
 import UrlPipe from 'src/pipes/urlPipe';
-import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
-import { rm } from 'fs';
-import { createTimestamp } from 'src/utils/utils';
+import { QueryData } from './twitter.interface';
 
 @Controller('api/screenshot/twitter')
 export class TwitterController {
-  constructor(
-    private readonly twitterService: TwitterService,
-    private readonly cloudinaryService: CloudinaryService,
-  ) {}
+  constructor(private readonly twitterService: TwitterService) {}
 
   @Get()
   @UsePipes(UrlPipe)
-  async getScreenshot(
-    @Query() query: { url: URL; commentsDepth: number },
-  ): Promise<string> {
-    const { url, commentsDepth } = query;
-    const split = url.pathname.split('/');
-    const userHandle = split[1];
-    const postId = split[3];
-    const browser: Browser = await puppeteer.launch({
-      headless: true,
-      defaultViewport: { width: 900, height: 3000 + commentsDepth * 1000 },
-    });
-    const page: Page = await browser.newPage();
-    await page.goto(url.href, {
-      waitUntil: 'networkidle0',
-      timeout: 30000,
-    });
-    await page.addStyleTag({
-      content:
-        '.css-175oi2r.r-aqfbo4.r-zchlnj.r-1d2f490.r-1xcajam.r-1p0dtai.r-12vffkv { display: none; }',
-    });
-    await page.waitForNetworkIdle({ concurrency: 2, timeout: 15000 });
-    await page.waitForSelector(
-      '.css-175oi2r[aria-label="Timeline: Conversation"] ',
-      { timeout: 15000 },
-    );
-    const domRect: DOMRect[] = await page.$$eval(
-      '.css-175oi2r[aria-label="Timeline: Conversation"] > div > div',
-      (array) => array.map((el) => el.getBoundingClientRect().toJSON()),
-    );
-    if (!domRect || !domRect.length) {
-      throw new HttpException(
-        'Provided url is incorrect or there is issue with Twitter.',
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-    const mainPost = domRect[0];
-    // const comments = domRect.slice(2);
-    // const commentsSlice =
-    //   commentsDepth > 0 ? comments.slice(0, commentsDepth) : [];
-    // const includedCommentsHeight: number =
-    //   commentsSlice.length > 0
-    //     ? comments
-    //         .slice(0, commentsDepth)
-    //         .map((c) => c.height)
-    //         .reduce((prev, curr) => prev + curr, 0)
-    //     : 0;
-    const fileName =
-      userHandle +
-      '_' +
-      postId +
-      // '_cd_' +
-      // commentsSlice.length +
-      '_' +
-      createTimestamp() +
-      '.jpeg';
-    const path = './temp/twitter/' + fileName;
-    await page.screenshot({
-      path: path,
-      quality: 100,
-      clip: {
-        width: mainPost.width + 40,
-        height:
-          mainPost.y + mainPost.height /* + includedCommentsHeight */ + 20,
-        x: mainPost.x - 20,
-        y: 0,
-      },
-    });
-    await browser.close();
-    const res = await this.cloudinaryService.saveToCloud(path);
-    rm(path, (error) => {
-      if (error) throw error;
-    });
-    return this.twitterService.sendURL(res);
+  async getScreenshot(@Query() query: QueryData): Promise<string> {
+    const urlData = this.twitterService.destructureQuery(query);
+    return this.twitterService.takeScreenshotOfPost(urlData);
   }
 }
--- src/screenshot/twitter/twitter.service.ts
+++ src/screenshot/twitter/twitter.service.ts
@@ -1,8 +1,84 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
+import { QueryData, UrlData } from './twitter.interface';
+import puppeteer, { Browser, Page } from 'puppeteer';
+import { createFilename } from 'src/utils/utils';
+import { rm } from 'fs';
 
 @Injectable()
 export class TwitterService {
-  sendURL(string: string): string {
-    return string;
+  constructor(private readonly cloudinaryService: CloudinaryService) {}
+
+  destructureQuery(query: QueryData): UrlData {
+    const { url, commentsDepth } = query;
+    const split = url.pathname.split('/');
+    const userHandle = split[1];
+    const postId = split[3];
+    return { url: url.href, userHandle, postId, commentsDepth };
+  }
+
+  async takeScreenshotOfPost(data: UrlData): Promise<string> {
+    const { url, userHandle, postId, commentsDepth } = data;
+    const browser: Browser = await puppeteer.launch({
+      headless: true,
+      defaultViewport: { width: 900, height: 3000 + commentsDepth * 1000 },
+    });
+    const page: Page = await browser.newPage();
+    await page.goto(url, {
+      waitUntil: 'networkidle0',
+      timeout: 30000,
+    });
+    await page.addStyleTag({
+      content:
+        '.css-175oi2r.r-aqfbo4.r-zchlnj.r-1d2f490.r-1xcajam.r-1p0dtai.r-12vffkv { display: none; }',
+    });
+    await page.waitForNetworkIdle({ concurrency: 2, timeout: 15000 });
+    await page.waitForSelector(
+      '.css-175oi2r[aria-label="Timeline: Conversation"] ',
+      { timeout: 15000 },
+    );
+    const domRect: DOMRect[] = await page.$$eval(
+      '.css-175oi2r[aria-label="Timeline: Conversation"] > div > div',
+      (array) => array.map((el) => el.getBoundingClientRect().toJSON()),
+    );
+    if (!domRect || domRect.length < 1) {
+      throw new HttpException(
+        'Provided url is incorrect or there is issue with Twitter.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const postRect: DOMRect = domRect[0];
+    const commentsRect: DOMRect[] = domRect.slice(2);
+    const commentsSlice: DOMRect[] = commentsRect.slice(0, commentsDepth);
+    const lastIncludedComment: DOMRect =
+      commentsSlice.length === 0
+        ? null
+        : commentsSlice[commentsSlice.length - 1];
+    const totalHeight: number =
+      lastIncludedComment === null
+        ? postRect.y + postRect.height
+        : lastIncludedComment.y + lastIncludedComment.height;
+
+    const fileName = createFilename(userHandle, postId, commentsSlice.length);
+    const path = './temp/twitter/' + fileName;
+
+    await page.screenshot({
+      path: path,
+      quality: 100,
+      clip: {
+        width: postRect.width + 40,
+        height: totalHeight + 20,
+        x: postRect.x - 20,
+        y: 0,
+      },
+    });
+    await browser.close();
+
+    const resUrl = await this.cloudinaryService.saveToCloud(path);
+    rm(path, (error) => {
+      if (error) throw error;
+    });
+    return resUrl;
   }
 }
