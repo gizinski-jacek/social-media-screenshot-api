5079ee3daec6192fb68fd32ac9ae4dbed30fb09f
Jacek Gizinski <jacektrg+github@gmail.com>
2025-02-26 18:17:11 +0100

    Configuring mongoose mongodb connection.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,20 @@ import { AppService } from './app.service';
 import { ScreenshotModule } from './screenshot/screenshot.module';
 import { ConfigModule } from '@nestjs/config';
 import { CloudinaryProvider } from './cloudinary/cloudinary.provider';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './mongo/users/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ScreenshotModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      process.env.NODE_ENV === 'production'
+        ? process.env.MONGO_DB_URI
+        : process.env.MONGO_DB_URI,
+    ),
+    ScreenshotModule,
+    UserModule,
+  ],
   controllers: [AppController, ApiController],
   providers: [AppService, CloudinaryProvider],
 })
