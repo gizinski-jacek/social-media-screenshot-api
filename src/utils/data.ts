b3be0e106cd93b9a7d5da83aee0e3d5496573382
Jacek Gizinski <jacektrg+github@gmail.com>
2025-02-26 18:30:11 +0100

    Adding data about supported social media site.

---
+++ src/utils/data.ts
@@ -0,0 +1,8 @@
+import { SupportedServices } from './types';
+
+export const supportedServicesData: SupportedServices = {
+  bsky: { dbArrayFieldName: 'blueskyScreenshots' },
+  twitter: { dbArrayFieldName: 'twitterScreenshots' },
+  facebook: { dbArrayFieldName: 'facebookScreenshots' },
+  instagram: { dbArrayFieldName: 'instagramScreenshots' },
+};
