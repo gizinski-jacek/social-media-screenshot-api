fcf87e36f5a8a830c6c1318c91d58b8fdf8a8cb4
Jacek Gizinski <jacektrg+github@gmail.com>
2025-03-11 19:50:55 +0100

    Refactoring into Post route.
    Using destructureUrl service to get necessary data from provided url to take screenshot, save the links and data to db and return screenshot link to client.

--- src/screenshot/facebook/facebook.controller.ts
+++ src/screenshot/facebook/facebook.controller.ts
@@ -1,30 +1,37 @@
-import {
-  Controller,
-  Get,
-  HttpException,
-  HttpStatus,
-  Query,
-  UsePipes,
-} from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { FacebookService } from './facebook.service';
 import UrlPipe from 'src/pipes/urlPipe';
-import { QueryData } from './facebook.interface';
+import { BodyPipedData } from 'src/utils/types';
+import { UserService } from 'src/mongo/users/user.service';
+import { Link } from 'src/mongo/schemas/link.schema';
 
 @Controller('api/screenshot/facebook')
 export class FacebookController {
-  constructor(private readonly facebookService: FacebookService) {}
+  constructor(
+    private readonly facebookService: FacebookService,
+    private readonly userService: UserService,
+  ) {}
 
-  @Get()
+  @Post()
   @UsePipes(UrlPipe)
-  async getScreenshot(@Query() query: QueryData): Promise<string> {
-    const urlData = this.facebookService.destructureQuery(query);
-
-    if (urlData.type === 'posts') {
-      return this.facebookService.takeScreenshotOfPost(urlData);
-    } else if (urlData.type === 'videos') {
-      return this.facebookService.takeScreenshotOfVideo(urlData);
-    } else {
-      throw new HttpException('Issue with url.', HttpStatus.BAD_REQUEST);
-    }
+  async getScreenshot(@Body() body: BodyPipedData): Promise<string> {
+    const urlData = await this.facebookService.destructureUrl(body);
+    const screenshotLink: string =
+      await this.facebookService.getScreenshot(urlData);
+    const screenshotData: Link = {
+      service: urlData.service,
+      originalPostUrl: urlData.originalPostUrl,
+      screenshotUrl: screenshotLink,
+      userHandle: urlData.userHandle,
+      postOwnerProfileLink: urlData.postOwnerProfileLink,
+      postId: urlData.postId,
+      commentsDepth: urlData.commentsDepth,
+      type: urlData.type,
+    };
+    await this.userService.upsertUser({
+      discordId: body.discordId,
+      postScreenshotData: screenshotData,
+    });
+    return screenshotLink;
   }
 }
