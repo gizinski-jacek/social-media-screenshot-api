6c544fcfb4d9846db3865919d43e406e33bbaf76
Jacek Gizinski <jacektrg+github@gmail.com>
2025-03-11 19:47:58 +0100

    Adding trycatch block.
    Throwing more appropriate error.

--- src/mongo/users/user.service.ts
+++ src/mongo/users/user.service.ts
@@ -1,5 +1,10 @@
 import { Model } from 'mongoose';
-import { Injectable, Dependencies } from '@nestjs/common';
+import {
+  Injectable,
+  Dependencies,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { getModelToken, InjectModel } from '@nestjs/mongoose';
 import { User } from '../schemas/user.schema';
 import { UpsertUserDto } from '../dto/upsert-user.dto';
@@ -13,15 +18,23 @@ export class UserService {
   }
 
   async upsertUser(upsertUserDto: UpsertUserDto): Promise<void> {
-    const arrayName = this.getArrayFieldName(
-      upsertUserDto.postScreenshotData.service,
-    );
-    await this.userModel.findOneAndUpdate(
-      { discordId: upsertUserDto.discordId },
-      { $push: { [arrayName]: upsertUserDto.postScreenshotData } },
-      { upsert: true, new: true },
-    );
-    return;
+    try {
+      const arrayName = this.getArrayFieldName(
+        upsertUserDto.postScreenshotData.service,
+      );
+      await this.userModel.findOneAndUpdate(
+        { discordId: upsertUserDto.discordId },
+        { $push: { [arrayName]: upsertUserDto.postScreenshotData } },
+        { upsert: true, new: true },
+      );
+      return;
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(
+        'Error saving screenshot data.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   getArrayFieldName(service: string): string {
