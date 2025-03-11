fdf9e2fc275d005c8c32f93c9d5c229d795968a4
Jacek Gizinski <jacektrg+github@gmail.com>
2025-03-11 19:51:14 +0100

    Updating BodyData type with nitter.

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,6 +2,7 @@ export interface BodyData {
   postUrl: string;
   commentsDepth: number;
   discordId: string;
+  nitter: boolean;
 }
 
 export interface BodyPipedData extends Omit<BodyData, 'postUrl'> {
