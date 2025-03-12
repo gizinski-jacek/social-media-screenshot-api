affafe74f628dbd9a503f5a66703d68386c4f829
Jacek Gizinski <jacektrg+github@gmail.com>
2025-03-12 23:06:46 +0100

    Setting puppeteers page user agent.
    Required for Nitter, otherwise page is blank (other fix was setting headless mode to false).
    For Twitter helps with post's media content sometimes not loading.
    Setting page's viewport before taking screenshot using content's height in case initial defaultViewport height is too small.

--- src/screenshot/twitter/twitter.service.ts
+++ src/screenshot/twitter/twitter.service.ts
@@ -41,13 +41,16 @@ export class TwitterService {
     const { postUrlData, userHandle, postId, commentsDepth } = data;
     const browser: Browser = await puppeteer.launch({
       headless: true,
-      defaultViewport: { width: 900, height: 3000 + commentsDepth * 1000 },
+      defaultViewport: { width: 900, height: 700 },
     });
     const page: Page = await browser.newPage();
     await page.goto(postUrlData.href, {
       waitUntil: 'networkidle0',
       timeout: 15000,
     });
+    await page.setUserAgent(
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+    );
     await page.addStyleTag({
       content:
         '.css-175oi2r.r-aqfbo4.r-zchlnj.r-1d2f490.r-1xcajam.r-1p0dtai.r-12vffkv { display: none; }',
@@ -82,6 +85,11 @@ export class TwitterService {
     const fileName = createFilename(userHandle, postId, commentsSlice.length);
     const path = './temp/twitter/' + fileName;
 
+    await page.setViewport({
+      width: 900,
+      height: Math.round(totalHeight + 20),
+      deviceScaleFactor: 1,
+    });
     await page.screenshot({
       path: path,
       quality: 100,
@@ -103,16 +111,22 @@ export class TwitterService {
     const { postUrlData, userHandle, postId, commentsDepth } = data;
     const browser: Browser = await puppeteer.launch({
       headless: true,
-      defaultViewport: { width: 800, height: 1800 },
+      defaultViewport: {
+        width: 700,
+        height: 500,
+      },
     });
     const page: Page = await browser.newPage();
+    await page.setUserAgent(
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+    );
     await page.goto(postUrlData.href, {
       waitUntil: 'networkidle0',
       timeout: 15000,
     });
     await page.waitForNetworkIdle({ concurrency: 2, timeout: 15000 });
-    await page.waitForSelector('#m', { timeout: 5000 });
-    const postRect: DOMRect = await page.$eval('#m', (el) =>
+    await page.waitForSelector('.main-tweet', { timeout: 5000 });
+    const postRect: DOMRect = await page.$eval('.main-tweet', (el) =>
       el.getBoundingClientRect().toJSON(),
     );
     if (!postRect) {
@@ -121,8 +135,9 @@ export class TwitterService {
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
-    const commentsRect: DOMRect[] = await page.$eval('#r', (el) =>
-      el.getBoundingClientRect().toJSON(),
+    const commentsRect: DOMRect[] = await page.$$eval(
+      '.reply.thread.thread-line',
+      (array) => array.map((el) => el.getBoundingClientRect().toJSON()),
     );
     const commentsSlice: DOMRect[] = commentsRect.slice(0, commentsDepth);
     const lastIncludedComment: DOMRect =
@@ -137,6 +152,11 @@ export class TwitterService {
     const fileName = createFilename(userHandle, postId, commentsSlice.length);
     const path = './temp/nitter/' + fileName;
 
+    await page.setViewport({
+      width: 700,
+      height: Math.round(totalHeight + 20),
+      deviceScaleFactor: 1,
+    });
     await page.screenshot({
       path: path,
       quality: 100,
