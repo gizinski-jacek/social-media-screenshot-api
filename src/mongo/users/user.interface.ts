c1fa66dc20f049f7d29802b7842fc18c841d5ec2
Jacek Gizinski <jacektrg+github@gmail.com>
2025-03-18 16:21:50 +0100

    Adding types for user route.

---
+++ src/mongo/users/user.interface.ts
@@ -0,0 +1,11 @@
+export interface UserBody {
+  discordId: string;
+  social: string;
+}
+
+export interface ScreenshotData {
+  url: string;
+  service: string;
+  userHandle: string;
+  date: string;
+}
