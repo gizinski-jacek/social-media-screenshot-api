733c66a635c41d0f6010ac040bcb132c8d830c9c
Jacek Gizinski <jacektrg+github@gmail.com>
2025-03-20 19:17:33 +0100

    Renaming route.
    Updating imports.
    Adding @Delete route for deleting recent screenshots.

--- src/mongo/users/user.controller.ts
+++ src/mongo/users/user.controller.ts
@@ -1,16 +1,27 @@
-import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import { Body, Controller, Delete, Post, UsePipes } from '@nestjs/common';
 import { UserService } from './user.service';
-import UserPipe from 'src/pipes/userPipe';
+import MostRecentScreenshotPipe from 'src/pipes/mostRecentScreenshotPipe';
 import { ScreenshotData, UserBody } from './user.interface';
 
 @Controller('api/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('last-screenshot')
-  @UsePipes(UserPipe)
-  async getLastScreenshot(@Body() body: UserBody): Promise<ScreenshotData> {
-    const data = await this.userService.getLastScreenshot(body);
+  @Post('recent-screenshot')
+  @UsePipes(MostRecentScreenshotPipe)
+  async getMostRecentScreenshot(
+    @Body() body: UserBody,
+  ): Promise<ScreenshotData> {
+    const data = await this.userService.getMostRecentScreenshot(body);
+    return data;
+  }
+
+  @Delete('recent-screenshot')
+  @UsePipes(MostRecentScreenshotPipe)
+  async deleteMostRecentScreenshot(
+    @Body() body: UserBody,
+  ): Promise<ScreenshotData> {
+    const data = await this.userService.deleteMostRecentScreenshot(body);
     return data;
   }
 }
