6f867f5032f167552add37f9b343b0324cf147ee
Jacek Gizinski <jacektrg+github@gmail.com>
2025-03-20 19:18:38 +0100

    Importing Cloudinary Service.

--- src/mongo/users/user.module.ts
+++ src/mongo/users/user.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '../schemas/user.schema';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [],
-  providers: [UserService],
+  providers: [UserService, CloudinaryService],
   exports: [
     UserService,
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
