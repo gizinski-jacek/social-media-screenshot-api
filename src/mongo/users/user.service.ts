fa800b9089006456c7f51c4d92fab19c9ca0c420
Jacek Gizinski <jacektrg+github@gmail.com>
2025-03-20 19:21:37 +0100

    Refactoring upsertUser.
    Adding getOrCreateUser method which gets user data from Db and returns it or creates it if user data does not exist.

--- src/mongo/users/user.service.ts
+++ src/mongo/users/user.service.ts
@@ -6,20 +6,50 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { getModelToken, InjectModel } from '@nestjs/mongoose';
-import { User } from '../schemas/user.schema';
-import { UpsertUserDto } from '../dto/upsert-user.dto';
+import { User, UserDocument } from '../schemas/user.schema';
+import { GetOrCreateUserDto } from '../dto/get-or-create-user.dto';
 import { supportedServicesData } from 'src/utils/data';
-import { Link } from '../schemas/link.schema';
+import { Link, LinkDocument } from '../schemas/link.schema';
 import { ScreenshotData, UserBody } from './user.interface';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
+import { v4 as uuidv4 } from 'uuid';
+import { UpdateUserDto } from '../dto/update-user.dto';
 
 @Injectable()
-@Dependencies(getModelToken(User.name))
+@Dependencies(getModelToken(User.name), CloudinaryService)
 export class UserService {
-  constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+    private readonly cloudinaryService: CloudinaryService,
+  ) {
     this.userModel = userModel;
   }
 
-  async upsertUser(upsertUserDto: UpsertUserDto): Promise<void> {
+  async getOrCreateUser(getOrCreateUser: GetOrCreateUserDto): Promise<User> {
+    try {
+      const userExists = await this.userModel.findOne({
+        discordId: getOrCreateUser.discordId,
+      });
+      if (!userExists) {
+        const cloudinaryId = uuidv4();
+        const newUser = await this.userModel.create({
+          discordId: getOrCreateUser.discordId,
+          cloudinaryId: cloudinaryId,
+        });
+        return newUser;
+      } else {
+        return userExists;
+      }
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(
+        'Error saving screenshot data.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async updateUser(upsertUserDto: UpdateUserDto): Promise<void> {
     try {
       const arrayName = this.getArrayFieldName(
         upsertUserDto.postScreenshotData.service,
@@ -27,9 +57,8 @@ export class UserService {
       await this.userModel.findOneAndUpdate(
         { discordId: upsertUserDto.discordId },
         { $push: { [arrayName]: upsertUserDto.postScreenshotData } },
-        { upsert: true, new: true },
+        { new: true },
       );
-      return;
     } catch (error) {
       console.log(error);
       throw new HttpException(
@@ -43,7 +72,7 @@ export class UserService {
     return supportedServicesData[service].dbArrayFieldName;
   }
 
-  async getLastScreenshot(data: UserBody): Promise<ScreenshotData> {
+  async getMostRecentScreenshot(data: UserBody): Promise<ScreenshotData> {
     const { discordId, social } = data;
     const user: User = await this.userModel.findOne({
       discordId: discordId,
@@ -60,14 +89,60 @@ export class UserService {
         ...user.twitterScreenshots,
       ];
     }
+    if (links.length === 0) {
+      throw new HttpException(
+        'No screenshots found.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
     const newest = links.sort(
-      (a, b) => b.createdAt.getTime() - a.createdAt.getTime(),
+      (a, b) => b.timestamp.getTime() - a.timestamp.getTime(),
     )[0];
     return {
       url: newest.screenshotUrl,
       service: newest.service,
       userHandle: newest.userHandle,
-      date: newest.createdAt.toISOString(),
+      timestamp: newest.timestamp.toISOString(),
+    };
+  }
+
+  async deleteMostRecentScreenshot(data: UserBody): Promise<ScreenshotData> {
+    const { discordId, social } = data;
+    const user: UserDocument = await this.userModel.findOne({
+      discordId: discordId,
+    });
+    let links: LinkDocument[] = [];
+    if (social) {
+      const arrayName = this.getArrayFieldName(social);
+      links = user[arrayName];
+    } else {
+      links = [
+        ...user.blueskyScreenshots,
+        ...user.facebookScreenshots,
+        ...user.instagramScreenshots,
+        ...user.twitterScreenshots,
+      ];
+    }
+    if (links.length === 0) {
+      throw new HttpException(
+        'No screenshots to delete.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+    const newest = links.sort(
+      (a, b) => b.timestamp.getTime() - a.timestamp.getTime(),
+    )[0];
+    const arrayName = this.getArrayFieldName(newest.service);
+    await this.userModel.findOneAndUpdate(
+      { discordId: discordId },
+      { $pull: { [arrayName]: { _id: newest._id } } },
+    );
+    await this.cloudinaryService.deleteFromCloud(newest.public_id);
+    return {
+      url: newest.screenshotUrl,
+      service: newest.service,
+      userHandle: newest.userHandle,
+      timestamp: newest.timestamp.toISOString(),
     };
   }
 }
