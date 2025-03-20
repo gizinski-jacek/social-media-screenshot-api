a38697761cda6363792b18614a8bfca80b1a1f43
Jacek Gizinski <jacektrg+github@gmail.com>
2025-03-20 19:11:51 +0100

    Adding 'secure' option.

--- src/cloudinary/cloudinary.provider.ts
+++ src/cloudinary/cloudinary.provider.ts
@@ -7,6 +7,7 @@ export const CloudinaryProvider = {
       cloud_name: process.env.CLOUDINARY_NAME,
       api_key: process.env.CLOUDINARY_API_KEY,
       api_secret: process.env.CLOUDINARY_API_SECRET,
+      secure: true,
     });
   },
 };
