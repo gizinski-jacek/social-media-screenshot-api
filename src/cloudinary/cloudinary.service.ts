8392cc931b0945e9a0b4d47234590c14bc900121
Jacek Gizinski <jacektrg+github@gmail.com>
2025-03-20 19:13:11 +0100

    Adding new parameters to save screenshots in user's subfolder and maintain consistent timestamp.
    Updating returned data.

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,20 +1,44 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryResponse } from './cloudinary.interface';
 
 @Injectable()
 export class CloudinaryService {
-  async saveToCloud(path: string): Promise<string> {
+  async saveToCloud(
+    filepath: string,
+    cloudinaryId: string,
+    timestamp: Date,
+  ): Promise<CloudinaryResponse> {
     try {
-      const res = await cloudinary.uploader.upload(path, {
+      const res = await cloudinary.uploader.upload(filepath, {
         use_filename: true,
         unique_filename: false,
-        overwrite: true,
+        folder: cloudinaryId,
+        format: 'jpg',
+        timestamp: timestamp.getTime(),
       });
-      return res.secure_url;
+      return {
+        public_id: res.public_id,
+        url: res.secure_url,
+        timestamp: timestamp,
+      };
     } catch (error: unknown) {
       console.log(error);
       throw new HttpException(
         'Error uploading screenshot.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async deleteFromCloud(publicId: string): Promise<string> {
+    try {
+      const res = await cloudinary.uploader.destroy(publicId);
+      return res;
+    } catch (error: unknown) {
+      console.log(error);
+      throw new HttpException(
+        'Error deleting screenshot.',
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
