f42bbc194893c5098a9934e134fb69395b975a86
Jacek Gizinski <jacektrg+github@gmail.com>
2025-03-24 20:26:36 +0100

    Adding routes for searching screenshots based on dates.

--- src/mongo/users/user.controller.ts
+++ src/mongo/users/user.controller.ts
@@ -1,7 +1,10 @@
 import { Body, Controller, Delete, Post, UsePipes } from '@nestjs/common';
 import { UserService } from './user.service';
 import MostRecentScreenshotPipe from 'src/pipes/mostRecentScreenshotPipe';
-import { ScreenshotData, UserBody } from './user.interface';
+import { ScreenshotData, UserBody, UserBodyPiped } from './user.interface';
+import ToDatePipe from 'src/pipes/toDatePipe';
+import FromDatePipe from 'src/pipes/fromDatePipe';
+import FromToDatePipe from 'src/pipes/fromToDatePipe';
 
 @Controller('api/user')
 export class UserController {
@@ -22,6 +25,33 @@ export class UserController {
     @Body() body: UserBody,
   ): Promise<ScreenshotData> {
     const data = await this.userService.deleteMostRecentScreenshot(body);
+    return data;
+  }
+
+  @Post('screenshot-from-to-date')
+  @UsePipes(FromToDatePipe)
+  async getScreenshotsFromToDate(
+    @Body() body: UserBodyPiped,
+  ): Promise<ScreenshotData[]> {
+    const data = await this.userService.getScreenshotsFromToDate(body);
+    return data;
+  }
+
+  @Post('screenshot-to-date')
+  @UsePipes(ToDatePipe)
+  async getScreenshotsToDate(
+    @Body() body: UserBodyPiped,
+  ): Promise<ScreenshotData[]> {
+    const data = await this.userService.getScreenshotsToDate(body);
+    return data;
+  }
+
+  @Post('screenshot-from-date')
+  @UsePipes(FromDatePipe)
+  async getScreenshotsFromDate(
+    @Body() body: UserBodyPiped,
+  ): Promise<ScreenshotData[]> {
+    const data = await this.userService.getScreenshotsFromDate(body);
     return data;
   }
 }
