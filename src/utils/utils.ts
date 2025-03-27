cec26d230bb3f7558da0d621410415f3ae79cf0e
Jacek Gizinski <jacektrg+github@gmail.com>
2025-03-27 20:07:40 +0100

    Adding function cleaning up directory by deleting screenshots files.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,14 +1,27 @@
+import { readdirSync, unlinkSync } from 'fs';
+import { join } from 'path';
+
 export const createFilename = (
+  service: string,
   handle: string,
   postId: string,
   timestamp: Date,
   cd?: number | undefined,
 ): string => {
-  const filename = `${handle}__${postId}__cd-${cd || 0}__${formatTimestamp(timestamp)}.jpeg`;
+  const filename = `${service}__${handle}__${postId}__cd-${cd || 0}__${formatTimestamp(timestamp)}.jpeg`;
   return filename;
 };
 
 export const formatTimestamp = (date: Date): string => {
   const timestamp = date.toISOString().replaceAll(/:|\./g, '-');
   return timestamp;
+};
+
+export const cleanUpDirectory = (dir: string) => {
+  const files = readdirSync(dir);
+  const filter = files.filter((file) => file !== '.gitignore');
+  for (const file of filter) {
+    console.log(file);
+    unlinkSync(join(dir, file));
+  }
 };
